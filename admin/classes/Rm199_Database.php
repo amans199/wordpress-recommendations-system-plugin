e545fa40058b3889d99972d0637dce6b2c34cd46
ahmed_mansour <ahmed.mansour@code95.info>
2020-08-13 05:58:53 +0200

    initial commit in database creation

--- admin/classes/Rm199_Database.php
+++ admin/classes/Rm199_Database.php
@@ -1,39 +1,39 @@
 <?php
-// if (!defined('ABSPATH')) {
-//     exit;
-// }
-// class Rm199Database
-// {
-//     public static function setup()
-//     {
-//         global $wpdb;
-//         $table_name = $wpdb->prefix . 'rm199_db';
-//         $charset_collate = $wpdb->get_charset_collate();
-//         $sql = "CREATE TABLE $table_name (
-//             id int(11) NOT NULL AUTO_INCREMENT,
-//             user_id int(11) NOT NULL,
-//             user_name text NOT NULL,
-//             last_login datetime DEFAULT '0000-00-00 00:00:00' NOT NULL,
-//             PRIMARY KEY (id)
-//             ) $charset_collate";
-//         require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
-//         dbDelta($sql);
-//     }
+if (!defined('ABSPATH')) {
+    exit;
+}
+class Rm199Database
+{
+    public static function setup()
+    {
+        global $wpdb;
+        $table_name = $wpdb->prefix . 'rm199_db';
+        $charset_collate = $wpdb->get_charset_collate();
+        $sql = "CREATE TABLE $table_name (
+            id int(11) NOT NULL AUTO_INCREMENT,
+            user_id int(11) NOT NULL,
+            user_name text NOT NULL,
+            last_login datetime DEFAULT '0000-00-00 00:00:00' NOT NULL,
+            PRIMARY KEY (id)
+            ) $charset_collate";
+        require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+        dbDelta($sql);
+    }
 
-//     public static function createDatabaseEntry($user, $current_user)
-//     {
-//         global $wpdb;
-//         $name = $current_user->user_login;
-//         $id = $current_user->ID;
-//         $table_name = $wpdb->prefix . 'rm199_db';
+    public static function createDatabaseEntry($user, $current_user)
+    {
+        global $wpdb;
+        $name = $current_user->user_login;
+        $id = $current_user->ID;
+        $table_name = $wpdb->prefix . 'rm199_db';
 
-//         $wpdb->insert(
-//             $table_name,
-//             array(
-//                 'user_id' => $id,
-//                 'user_name' => $name,
-//                 'last_login' => current_time('mysql')
-//             )
-//         );
-//     }
-// }
+        $wpdb->insert(
+            $table_name,
+            array(
+                'user_id' => $id,
+                'user_name' => $name,
+                'last_login' => current_time('mysql')
+            )
+        );
+    }
+}
