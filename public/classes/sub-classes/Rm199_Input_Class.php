e388c73c1d5c4251efefb0fa4b64e2827d3e802e
amans199 <ahmed.ouda1997@gmail.com>
2020-12-23 22:42:52 +0200

    #load_via_ajax_mission_1_3 : load categories inside preferences via ajax

--- public/classes/sub-classes/Rm199_Input_Class.php
+++ public/classes/sub-classes/Rm199_Input_Class.php
@@ -21,7 +21,7 @@ class Rm199Input
             <!-- todo :: add templates :: topbar , side notice , send email , etc...  -->
             <div class="rm199_topbar">
                 <p class="rm199_preferences_example__txt"><?php _e('Preferences helps us to provide you with the best experience', 'rm199'); ?></p>
-                <a href="#" id="rm199_preferences_modal_btn"><?php _e('Add Preferences', 'rm199'); ?></a>
+                <a href="#" id="rm199_preferences_modal_btn" onclick="add_preferences_handler()"><?php _e('Add Preferences', 'rm199'); ?></a>
             </div>
             <div id="rm199_preferences_modal" class="modal">
                 <div class="modal-content">
@@ -32,7 +32,9 @@ class Rm199Input
                         <span class="close">&times;</span>
                     </div>
                     <div class="modal-body">
-
+                        <div class="modal_loader">
+                            <span class="icon dashicons dashicons-update"></span>
+                        </div>
                         <div class="rm199_modal_cards">
 
                             <!-- preferences cards :: credits to mednabouli -> https://codepen.io/mednabouli/pen/KoRmyE -->
@@ -40,23 +42,29 @@ class Rm199Input
                             <h3> What do you like the most !? </h3>
 
                             <div class="grid-wrapper">
-                                <?php $current_user_preferences = get_user_meta(get_current_user_id(), 'preferences', false);
-                                foreach (get_categories() as $category) {
-                                    if ($category->count > 0) {
+                                <?php // $current_user_preferences = get_user_meta(get_current_user_id(), 'preferences', false);
+                                // foreach (get_categories() as $category) {
+                                //     if ($category->count > 0) {
                                 ?>
-                                        <div class="card-wrapper">
-                                            <input class="c-card" type="checkbox" id="<?php echo  $category->term_id; ?>" value="<?php echo $category->cat_name; ?>" <?php echo (in_array($category->cat_name, $current_user_preferences) ? "checked" : ''); ?>>
+                                <!-- <div class="card-wrapper">
+                                            <input class="c-card" type="checkbox" id="<?php // echo  $category->term_id; 
+                                                                                        ?>" value="<?php // echo $category->cat_name; 
+                                                                                                    ?>" <?php //echo (in_array($category->cat_name, $current_user_preferences) ? "checked" : ''); 
+                                                                                                        ?>>
                                             <div class="card-content">
                                                 <div class="card-state-icon"></div>
-                                                <label for="<?php echo  $category->term_id; ?>">
-                                                    <h4><?php echo $category->cat_name; ?></h4>
+                                                <label for="<?php // echo  $category->term_id; 
+                                                            ?>">
+                                                    <h4><?php // echo $category->cat_name; 
+                                                        ?></h4>
                                                 </label>
                                             </div>
-                                        </div>
+                                        </div> -->
 
                                 <?php
-                                    }
-                                } ?>
+                                //    }
+                                //    } 
+                                ?>
                             </div>
                         </div>
                     </div>
--- public/public.php
+++ public/public.php
@@ -133,3 +133,79 @@ function process_post()
     }
 }
 add_action('init', 'process_post');
+
+
+
+/**
+ * Fetch all keywords
+ */
+function rm199_fetch_keywords()
+{
+
+    $plugin_url  = plugins_url() . '/recommendations-master';     // Used to keep our Template Directory URL
+    $ajax_url   = admin_url('admin-ajax.php');        // Localized AJAX URL
+
+    // Register Our Script for Localization
+    wp_register_script(
+        'fetch_all_keywords',
+        "{$plugin_url}/public/js/fetch_all_keywords.js",
+        array('jquery'),
+        '1.0',
+        true
+    );
+    $current_user_preferences = get_user_meta(get_current_user_id(), 'preferences', false);
+    // Localize Our Script so we can use `ajax_url`
+    wp_localize_script(
+        'fetch_all_keywords',
+        'rm199Obj',
+        array(
+            'ajax_url' => $ajax_url,
+            'security'  => wp_create_nonce('rm199'),
+            'siteurl'  => get_site_url(),
+            'user' => get_current_user_id(),
+            'user_preferences' => $current_user_preferences,
+            'all_categories' => get_categories()
+        )
+
+    );
+
+    // Finally enqueue our script
+    wp_enqueue_script('fetch_all_keywords');
+}
+add_action('wp_enqueue_scripts', 'rm199_fetch_keywords');
+
+/**
+ * AJAX Callback
+ * Always Echos and Exits
+ */
+function rm199_fetch_keywords_callback()
+{
+
+    // Ensure we have the data we need to continue
+    if (!isset($_POST) || empty($_POST) || !is_user_logged_in()) {
+
+        // If we don't - return custom error message and exit
+        header('HTTP/1.1 400 Empty POST Values');
+        echo 'Could Not Verify POST Values.';
+        exit;
+    }
+
+    // $user_id                        = get_current_user_id();
+    // delete_user_meta($user_id, 'preferences');
+
+    // $arr_of_preferences       = array_filter($_POST['preferences']);
+    // $arr_of_preferences_sanitized = [];
+    // foreach ($arr_of_preferences as $el) {
+    //     $value_sanitized = sanitize_text_field(wp_strip_all_tags($el));
+    //     if ($value_sanitized !== '' && !in_array($value_sanitized, $arr_of_preferences_sanitized)) {
+    //         array_push($arr_of_preferences_sanitized,  $value_sanitized);
+    //         add_user_meta($user_id, 'preferences', $value_sanitized);
+    //     }
+    // }
+    echo 'aloha';
+
+
+    exit;
+}
+add_action('wp_ajax_nopriv_fetch_keywords_cb', 'rm199_fetch_keywords_callback');
+add_action('wp_ajax_fetch_keywords_cb', 'rm199_fetch_keywords_callback');
