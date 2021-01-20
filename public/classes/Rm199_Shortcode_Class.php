f55f4105bf012649caa845bd693f67898af4208b
amans199 <ahmed.ouda1997@gmail.com>
2021-01-20 08:40:20 +0200

    #add-feature : input for users to edit their preferences

--- public/classes/Rm199_Shortcode_Class.php
+++ public/classes/Rm199_Shortcode_Class.php
@@ -14,9 +14,14 @@ class Rm199ShortCodeManager
     // todo :: make rm199_input doesn't show for not logged in && fix its issues
     public static function rm199_input($attr)
     {
-        include_once('sub-classes/Rm199_Input_Class.php');
-        $Rm199Input = new Rm199Input();
+        // include_once('sub-classes/Rm199_Input_Class.php');
+        // $Rm199Input = new Rm199Input();
+        // $Rm199Input->rm199_input($attr);
+        ob_start();
+        include_once('sub-classes/Rm199_edit_preferences_handler_Class.php');
+        $Rm199Input = new Rm199_edit_preferences_handler_Class();
         $Rm199Input->rm199_input($attr);
+        return ob_get_clean();
     }
     public static function rm199_posts($attr)
     {
